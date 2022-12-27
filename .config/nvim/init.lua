bbfc105279bfe7a4ec40517d2a7821e91fcd4927
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2022-12-26 21:45:57 -0500

    enable gopls

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -395,8 +395,8 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
+  gopls = {},
   -- clangd = {},
-  -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
