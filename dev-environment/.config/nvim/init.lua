253a34c7e52f56b491019ecdc0e3552d06ef7ebe
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-01-09 17:21:05 -0500

    remove <tab> / <s-tab> binds for autocomplete

--- dev-environment/.config/nvim/init.lua
+++ dev-environment/.config/nvim/init.lua
@@ -479,24 +479,6 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
   },
   sources = {
     { name = 'nvim_lsp' },
