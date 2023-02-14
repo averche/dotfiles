4fe90bba6536ec638af9b5849d9291ac01b6a7fa
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-02-14 15:49:26 -0500

    no rel numbers

--- dotfiles/.config/nvim/lua/config/options.lua
+++ dotfiles/.config/nvim/lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.wo.relativenumber = true
+vim.wo.relativenumber = false
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
