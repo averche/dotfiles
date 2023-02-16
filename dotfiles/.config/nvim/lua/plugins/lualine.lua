83ae1088006c7899d530c5b2bf19e8b5e41d4757
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-02-16 17:07:09 -0500

    lualine

---
+++ dotfiles/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    sections = {
+      lualine_y = {
+        { "progress", separator = "", padding = { left = 1, right = 1 } },
+      },
+      lualine_z = {
+        { "location", padding = { left = 0, right = 1 } },
+      },
+    },
+  },
+}
