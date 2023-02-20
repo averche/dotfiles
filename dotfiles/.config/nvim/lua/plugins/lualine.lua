0519bed744bb1deaf3fbc31c816204d20b8bba4b
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-02-20 17:57:46 -0500

    remove redundant padding

--- dotfiles/.config/nvim/lua/plugins/lualine.lua
+++ dotfiles/.config/nvim/lua/plugins/lualine.lua
@@ -3,10 +3,10 @@ return {
   opts = {
     sections = {
       lualine_y = {
-        { "progress", separator = "", padding = { left = 1, right = 1 } },
+        { "progress" },
       },
       lualine_z = {
-        { "location", padding = { left = 0, right = 1 } },
+        { "location" },
       },
     },
   },
