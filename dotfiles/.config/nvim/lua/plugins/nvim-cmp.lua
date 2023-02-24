8ada54f9a71872a6d4417e320330ba972a2c8907
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-02-23 20:03:15 -0500

    config nvim-cmp

--- dotfiles/.config/nvim/lua/plugins/nvim-cmp.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -5,5 +5,12 @@ return {
       completion = require("cmp").config.window.bordered(),
       documentation = require("cmp").config.window.bordered(),
     },
+    sources = {
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+    },
+    experimental = {},
   },
 }
+
+-- vim: ts=2 sts=2 sw=2 et
