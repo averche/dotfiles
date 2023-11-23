f555b8660bfd5100c9436b6b898d29b7e84f202b
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-11-23 18:31:15 -0500

    lsp keymaps comment

--- dotfiles/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,11 @@
+-- https://www.lazyvim.org/plugins/lsp#%EF%B8%8F-customizing-lsp-keymaps
+
 return {
   "neovim/nvim-lspconfig",
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     keys[#keys + 1] = {
+      -- remap 'gr' to the modified lsp_references function
       "gr",
       function()
         require("telescope.builtin").lsp_references({
