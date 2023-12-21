fe0d3dd1044130085a4d18bf1727f9691ffaa92f
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-12-21 18:41:14 -0500

    better lsp config for go

---
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,42 @@
+return {
+  -- add to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "go" })
+      end
+    end,
+  },
+
+  -- set up lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = {
+        -- remap 'gr' to the modified lsp_references function
+        "gr",
+        function()
+          require("telescope.builtin").lsp_references({
+            show_line = false,
+          })
+        end,
+      }
+    end,
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        gopls = {
+          completeUnimported = true,
+          usePlaceholders = true,
+          analyses = {
+            unusedparams = true,
+          },
+        },
+      },
+    },
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
