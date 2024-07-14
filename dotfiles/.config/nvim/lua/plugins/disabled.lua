02e803d8153e067b49cbf31895bf7e8ad7216904
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2024-07-13 22:54:18 -0400

    disable noice

--- dotfiles/.config/nvim/lua/plugins/disabled.lua
+++ dotfiles/.config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,7 @@
 return {
   { "rafamadriz/friendly-snippets", enabled = false },
   { "folke/todo-comments.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
