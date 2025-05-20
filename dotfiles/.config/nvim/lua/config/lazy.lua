635cb6683b6f0ee1f1416d1858084efdbde82b8c
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2025-05-19 22:57:12 -0400

    Revert "switch to the default Go support"
    This reverts commit 7a98f6b1a2830db1bf4686550a57f0598be166da.

--- dotfiles/.config/nvim/lua/config/lazy.lua
+++ dotfiles/.config/nvim/lua/config/lazy.lua
@@ -12,7 +12,6 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.rust" },
