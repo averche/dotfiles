7a98f6b1a2830db1bf4686550a57f0598be166da
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2025-05-19 22:46:56 -0400

    switch to the default Go support

--- dotfiles/.config/nvim/lua/config/lazy.lua
+++ dotfiles/.config/nvim/lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.rust" },
