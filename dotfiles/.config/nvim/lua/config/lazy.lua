26f0bfd6a060d4a087d5644f41960a807e91b876
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2025-05-26 17:02:49 -0400

    add sql support

--- dotfiles/.config/nvim/lua/config/lazy.lua
+++ dotfiles/.config/nvim/lua/config/lazy.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
