d2f5b35ec5668310c2436845bdf04b71db6f67bf
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2025-05-26 17:06:21 -0400

    Revert "add sql support"
    This reverts commit 26f0bfd6a060d4a087d5644f41960a807e91b876.

--- dotfiles/.config/nvim/lua/config/lazy.lua
+++ dotfiles/.config/nvim/lua/config/lazy.lua
@@ -15,7 +15,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
