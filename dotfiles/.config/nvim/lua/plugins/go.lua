e81aa4bbec8bedea80147ef3730ee225563b424d
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2025-10-09 12:50:58 -0400

    fix ref

--- dotfiles/.config/nvim/lua/plugins/go.lua
+++ dotfiles/.config/nvim/lua/plugins/go.lua
@@ -146,7 +146,7 @@ return {
 
   -- Filetype icons
   {
-    "echasnovski/mini.icons",
+    "nvim-mini/mini.icons",
     opts = {
       file = {
         [".go-version"] = { glyph = "", hl = "MiniIconsBlue" },
